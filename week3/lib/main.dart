c71135cd8d77c2e14b406f339a0f5cbecdaea480
Veronika L <verokipri@yahoo.com>
2022-02-05 23:20:27 +0100

    week3

---
+++ week3/lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+  runApp(
+    MaterialApp(
+      title: "Composition",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Basics of composition",), centerTitle: true,
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          color: Colors.lightBlue,
+          height: double.infinity,
+          width: double.infinity,
+          padding: const EdgeInsets.all(15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  YellowBalloon(),
+                  YellowBalloon(),
+                  RedBalloon(),
+                  YellowBalloon()
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  YellowBalloon(),
+                  RedBalloon(),
+                  RedBalloon(),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  RedBalloon(),
+                  YellowBalloon(),
+                  RedBalloon(),
+                  YellowBalloon()
+                ],
+              ),
+            ],),) ,
+      ),
+  ));
+}
+
+class YellowBalloon extends StatelessWidget {
+  YellowBalloon({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        border: Border.all(),
+        borderRadius: BorderRadius.circular(50.0),
+      ),
+    );
+  }
+}
+
+class RedBalloon extends StatelessWidget {
+  RedBalloon({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(
+        color: Colors.red,
+        border: Border.all(),
+        borderRadius: BorderRadius.circular(50.0),
+      ),
+    );
+  }
+}
