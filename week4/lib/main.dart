3106816bbe5bd4bafe74d74abb3f6f2ebcd6a59a
Veronika L <verokipri@yahoo.com>
2022-02-12 01:34:14 +0100

    added SizedBox

--- week4/lib/main.dart
+++ week4/lib/main.dart
@@ -53,46 +53,52 @@ void decrement(){
         ),
         body: Center(
           child: SizedBox(
-            width: 300,
+            width: 320,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                FloatingActionButton.small(
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                  FloatingActionButton.small(
                   backgroundColor: Colors.pink,
                   onPressed: () {
                   decrement();
                   },
-
                   child: const Icon(
                     Icons.remove,
                     color: Colors.white,
                     ),
                 ),
-                TextField(
-                  controller: TextEditingController()..text = fontSize.toString(),
-                  decoration: const InputDecoration(labelText: "Размер шрифта", border: OutlineInputBorder(),),
-                  keyboardType: TextInputType.number,
-                  textAlign: TextAlign.center,
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.digitsOnly
-                  ], // Only numbers can be entered
-                  onSubmitted: (String font) {
+                  SizedBox(
+                    width: 200,
+                    child: TextField(
+                      controller: TextEditingController()..text = fontSize.toString(),
+                      decoration: const InputDecoration(labelText: "Размер шрифта", border: OutlineInputBorder(),),
+                      keyboardType: TextInputType.number,
+                      textAlign: TextAlign.center,
+                      inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                      ], // Only numbers can be entered
+                      onSubmitted: (String font) {
 										double newFont = double.parse(font);
                     setState(() {
                       fontSize = newFont;
                     });
                   },
-                ),
-                FloatingActionButton.small(
+                ), 
+                  ),
+                  FloatingActionButton.small(
                   backgroundColor: Colors.pink,
                   onPressed: () {
                     increment();
                   },
-
                   child: const Icon(
                     Icons.add,
                     color: Colors.white,
                     ),
+                ),
+                  ],
                 ),
                 Text("Винни-пух и пятачок отдыхали на веточке дуба. Пух сказал: ”Интересно, как долго мы ещё будем здесь торчать?” Пятачок ответил: ” Я думаю, ещё лет десять.",
                     style: TextStyle(fontSize: fontSize, fontWeight: FontWeight.bold)
