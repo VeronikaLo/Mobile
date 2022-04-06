174f87de4c7f6d07b270743c8f060a11543a68af
Veronika L <verokipri@yahoo.com>
2022-04-06 22:21:30 +0200

    changed images from Networt to Assets

--- week9/lib/main.dart
+++ week9/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-//import 'package:week9/navigation.dart';
+
+
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Passing Data',
+      title: 'Passing data with inherited widget',
       home: Home(),
     );
   }
@@ -27,8 +28,6 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-//inherited widget
-
 class _HomeState extends State<Home> {
   late bool _isOn = false;
   @override
@@ -42,8 +41,8 @@ class _HomeState extends State<Home> {
             const Text('\u{1F466}'),
             Switch(
               value: _isOn,
-              activeTrackColor: Colors.purple[200],
-              activeColor: Colors.purple[500],
+              activeTrackColor: Colors.brown[200],
+              activeColor: Colors.brown,
               onChanged: (bool value){
                 setState(()=> _isOn = value);
               }),
@@ -51,24 +50,24 @@ class _HomeState extends State<Home> {
           ],
         ) ,
       ),
-      body: SuperState(superheroState: _isOn, child: const Hero()),
+      body: SuperState(heroState: _isOn, child: const Hero()),
     );
   }
 }
 
-
+//inherited widget
 class SuperState extends InheritedWidget {
-  final bool superheroState;
+  final bool heroState;
 
-const SuperState({ Key? key, required this.superheroState, required Widget child})
+const SuperState({ Key? key, required this.heroState, required Widget child})
       : super(child: child);
 
   @override
   bool updateShouldNotify(covariant SuperState oldWidget) =>
-      oldWidget.superheroState != superheroState;
+      oldWidget.heroState != heroState;
 
   static bool? of(BuildContext context) =>
-      context.dependOnInheritedWidgetOfExactType<SuperState>()!.superheroState;
+      context.dependOnInheritedWidgetOfExactType<SuperState>()!.heroState;
 }
 
 
@@ -104,41 +103,42 @@ class _NavBarState extends State<NavBar> {
 */
 class Hero extends StatelessWidget {
   const Hero({ Key? key }) : super(key: key);
-
   
+  final String image1 = "assets/images/Banner.webp";
+  final String image2 = "assets/images/hulk.jfif";
 
-  final String image1 = "https://static.wikia.nocookie.net/avengers/images/9/90/Bruce_Banner_Avengers_2.png/revision/latest/scale-to-width-down/505?cb=20141019124251&path-prefix=de";
-  final String image2 = "https://comicvine.gamespot.com/a/uploads/original/11123/111230072/6000203-7447360750-latest";
 
   @override
   Widget build(BuildContext context) {
-    final superheroState = SuperState.of(context) ?? false;
+    final heroState = SuperState.of(context) ?? false;
     return Stack(
       
       alignment: Alignment.bottomCenter,
       children: [
-        
         Container(
           height: double.infinity,
           decoration: BoxDecoration(
-          image: DecorationImage(image: NetworkImage(superheroState ? image2 : image1),
-          fit: BoxFit.cover,
-          )
+          image: DecorationImage(
+            image: AssetImage(heroState ? image2 : image1),
+            fit: BoxFit.cover,
+            )
           ),
         ),
+
         Container(
           height: 130,
           width: 200,
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(20.0)),
-            color: Colors.brown,
+          decoration:  BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+            color: heroState ? Colors.amberAccent : Colors.indigo[200],
           ),
           margin: const EdgeInsets.only(bottom: 30.0) ,
           padding: const EdgeInsets.all(10),
           child: Column(children:  [
-            Text(superheroState ? "Hulk" : "Bruce Banner",
-                  style: const TextStyle(fontSize: 25.0),),
-            const Text('aaaa'),
+            Text(heroState ? "Hulk" : "Bruce Banner",
+                  style: const TextStyle(fontSize: 25.0, color: Colors.white ),),
+            Text(heroState ? "a green-skinned, hulking and muscular humanoid with limitless  physical strength" : "a genius scientist, physicist, and medical doctor with seven Ph.D.s",
+            textAlign: TextAlign.center,),
           ],)
             
         ),
