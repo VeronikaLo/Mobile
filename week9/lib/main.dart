a83f0a337dca118e0580e25d98de1f5d9a045c30
Veronika L <verokipri@yahoo.com>
2022-04-10 00:21:33 +0200

    removed drafts

--- week9/lib/main.dart
+++ week9/lib/main.dart
@@ -67,36 +67,7 @@ class SuperState extends InheritedWidget {
       context.dependOnInheritedWidgetOfExactType<SuperState>()!.heroState;
 }
 
-/*
-class NavBar extends StatefulWidget {
-  const NavBar({ Key? key }) : super(key: key);
 
-  @override
-  State<NavBar> createState() => _NavBarState();
-}
-
-class _NavBarState extends State<NavBar> {
-  bool _isOn = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:  [
-            const Text('\u{1F466}'),
-            Switch(
-              value: _isOn,
-              activeTrackColor: Colors.purple[200],
-              activeColor: Colors.purple[500],
-              onChanged: (bool value){
-                setState(()=> _isOn = value);
-              }),
-            const Text('\u{1F47F}'),
-          ],
-        );
-  }
-}
-*/
 class Hero extends StatelessWidget {
   const Hero({Key? key}) : super(key: key);
 
